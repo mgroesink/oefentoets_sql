d29b9f5b2b9de16773ce9f609fed580a6c8bb677
Marcel Roesink <mroesink@rocvantwente.nl>
2022-11-04 06:21:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# I1SD_oefentoets_sql
+Veel succes!
---
+++ oefentoets_i1sd.sql
@@ -0,0 +1,21 @@
+/*
+OPDRACHT 1
+Toon de naam van alle fietsen van modeljaar 2017 die duurder zijn dan 1000 dollar.
+Toon de duurste fietsen als eerste.
+(85 rijen)
+*/
+
+/*
+OPDRACHT 2
+Maak een overzicht dat van alle stafleden (staff) die geen manager hebben de volledige naam laat zien.
+Toon ook de naam van de winkel (store) waar ze werkzaam zijn.
+(1 rij)
+*/
+
+/*
+OPDRACHT 3
+Maak een overzicht dat de volledige naam toont van alle werknemers en de volledige naam
+van de manager.
+Gebruik als kolomkoppen respectievelijk Werknemer en Manager
+(10 rijen)
+*/
